0f94727b255b2e777bd79ad8ed547da62fe200cd
btanmay192 <btanmay192@gmail.com>
2021-07-09 23:29:07 +0530

    Setup Spring boot initializer for the project.

---
+++ src/main/java/com/tanmay192/CoronaVirusTracker/CoronaVirusTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.tanmay192.CoronaVirusTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoronaVirusTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoronaVirusTrackerApplication.class, args);
+	}
+
+}
